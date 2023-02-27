641d63c5a45193653b02989f83b46548d138252a
CyberNat2000 <77163754+CyberNat2000@users.noreply.github.com>
2023-02-27 13:26:43 +0100

    Create zad1.cpp

---
+++ lekcja 1/zad1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dzielna;
+    cin >> dzielna;
+    int dzielnik;
+    cin >> dzielnik;
+    cout << dzielna/dzielnik << " reszta " << dzielna%dzielnik << endl;
+    return 0;
+}
