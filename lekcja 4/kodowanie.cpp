fa027685f388eaad8074b3fa18c5ce4d0c2667c5
CyberNat2000 <77163754+CyberNat2000@users.noreply.github.com>
2023-04-24 13:05:08 +0200

    Update kodowanie.cpp

--- lekcja 4/kodowanie.cpp
+++ lekcja 4/kodowanie.cpp
@@ -17,17 +17,21 @@ int main(){
     int liczba;
     string szyfr;
     cout<<"Podaj zdanie do szyfrowania"<<endl;
-    cin>>wiadomosc;
+    getline(cin, wiadomosc);
     cout<<"Podaj liczbę mniejszą od 26"<<endl;
     cin>>liczba;
     for(int i=0; i<wiadomosc.length(); i++){
+        if(toupper(wiadomosc[i])<91&&toupper(wiadomosc[i])>64)
         szyfr+= kodowanie(wiadomosc[i], liczba);
+        else szyfr+= wiadomosc[i];
     }
     cout<<"zaszyfrowana wiadomośc to: "<<szyfr<<endl;
     string Szyfr=szyfr;
     szyfr= "";
     for(int i=0; i<Szyfr.length(); i++){
+        if(toupper(wiadomosc[i])<91&&toupper(wiadomosc[i])>64)
         szyfr+= dekodowanie(Szyfr[i], liczba);
+        else szyfr+= Szyfr[i];
     }
     cout<<"zdekodowana wiadomośc to: "<<szyfr<<endl;
 }
